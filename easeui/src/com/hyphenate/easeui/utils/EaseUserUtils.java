ac5811ea117cf177832fc39e68f4553ba2bd97f3
Fox <279099862@qq.com>
2017-06-10 22:20:27 +0800

    add easeui src/res

--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -10,6 +10,7 @@ import com.hyphenate.easeui.R;
 import com.hyphenate.easeui.controller.EaseUI;
 import com.hyphenate.easeui.controller.EaseUI.EaseUserProfileProvider;
 import com.hyphenate.easeui.domain.EaseUser;
+import com.hyphenate.easeui.domain.User;
 
 public class EaseUserUtils {
     
@@ -30,7 +31,18 @@ public class EaseUserUtils {
         
         return null;
     }
-    
+    /**
+     * get User according username
+     * @param username
+     * @return
+     */
+    public static User getAppUserInfo(String username){
+        if(userProvider != null)
+            return userProvider.getAppUser(username);
+
+        return null;
+    }
+
     /**
      * set user avatar
      * @param username
@@ -63,5 +75,64 @@ public class EaseUserUtils {
         	}
         }
     }
-    
+    /**
+     * set user avatar
+     * @param username
+     */
+    public static void setAppUserAvatar(Context context, String username, ImageView imageView){
+    	User user = getAppUserInfo(username);
+        setAppUserAvatar(context, user, imageView);
+
+    }
+    /**
+     * set user avatar
+     * @param user
+     */
+    public static void setAppUserAvatar(Context context, User user, ImageView imageView){
+        if(user != null && user.getAvatar() != null){
+            showAvatar(context, user.getAvatar(), imageView);
+        }else{
+            Glide.with(context).load(R.drawable.default_hd_avatar).into(imageView);
+        }
+    }
+
+    public static void showAvatar(Context context, String avatarPath, ImageView imageView) {
+        if (avatarPath != null) {
+            try {
+                int avatarResId = Integer.parseInt(avatarPath);
+                Glide.with(context).load(avatarResId).into(imageView);
+            } catch (Exception e) {
+                //use default avatar
+                Glide.with(context).load(avatarPath).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.default_hd_avatar).into(imageView);
+            }
+        } else {
+            Glide.with(context).load(R.drawable.default_hd_avatar).into(imageView);
+        }
+    }
+
+
+    /**
+     * set user's nickname
+     */
+    public static void setAppUserNick(String username,TextView textView){
+
+        if(textView != null){
+        	User user = getAppUserInfo(username);
+        	setAppUserNick(user, textView);
+        }
+    }
+    /**
+     * set user's nickname
+     */
+    public static void setAppUserNick(User user,TextView textView){
+
+        if(textView != null&& user!=null){
+        	if(user.getMUserNick() != null){
+        		textView.setText(user.getMUserNick());
+        	}else{
+        		textView.setText(user.getMUserName());
+        	}
+        }
+    }
+
 }
